2bc63366c1b9c353df321bda00fb6add1ddf4ea8
narcissusdu <narcissusdu@gmail.com>
2016-11-17 18:22:37 +0800

    First commit

---
+++ src/Interfaces/AbstractTestClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestInterfaces
+{
+    public abstract class AbstractTestClass : ITestable
+    {
+        public abstract string GetName();
+
+        public abstract string Test();
+
+    }
+}
