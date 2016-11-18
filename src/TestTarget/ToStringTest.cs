ac8b0b404cc0ac8add85a99e77166b9aa923414d
narcissusdu <narcissusdu@gmail.com>
2016-11-18 04:03:19 +0000

    added a test

---
+++ src/TestTarget/ToStringTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TestInterfaces;
+
+namespace TestTarget
+{
+    public class ToStringTest : AbstractTestClass
+    {
+        public override string Test()
+        {
+	    return DateTime.Now.ToString("YYYY-mm-DD");
+	}
+    }
+}
